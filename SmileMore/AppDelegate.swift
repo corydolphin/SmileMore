91371d2801e5f5e939eb725c9aa45da6196b223d
Cory Dolphin <corydolphin@gmail.com>
2016-01-25 20:45:07 -0800

    Initial Commit

---
+++ SmileMore/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SmileMore
+//
+//  Created by Cory Dolphin on 1/25/16.
+//  Copyright © 2016 Dolphin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SmileMore/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SmileMore
+//
+//  Created by Cory Dolphin on 1/25/16.
+//  Copyright © 2016 Dolphin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
