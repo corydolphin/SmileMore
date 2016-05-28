f918d6cd2ccbbbece0b1700822331b2925daddf1
Cory Dolphin <corydolphin@gmail.com>
2016-05-28 15:12:38 -0700

    WIP: Leaking like Snowden

--- SmileMore/ViewController.swift
+++ SmileMore/ViewController.swift
@@ -12,9 +12,9 @@ import AVFoundation
 class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDelegate {
     var previewLayer: AVCaptureVideoPreviewLayer?
     var session: AVCaptureSession?
+    var detector: CIDetector?
     let videoDelegateQueue = dispatch_queue_create("VideoDataOutputQueue", DISPATCH_QUEUE_SERIAL)
-    let context = CIContext()
-    let detector = CIDetector.init(ofType: CIDetectorTypeFace, context: nil, options: [CIDetectorAccuracy: CIDetectorAccuracyLow])
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -39,14 +39,10 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
         let videoOutput = AVCaptureVideoDataOutput.init()
         videoOutput.alwaysDiscardsLateVideoFrames = true
         videoOutput.setSampleBufferDelegate(self, queue: videoDelegateQueue)
-
         session?.addOutput(videoOutput)
-
         session?.startRunning()
     }
 
-
-    
     override var representedObject: AnyObject? {
         didSet {
         // Update the view, if already loaded.
@@ -54,19 +50,24 @@ class ViewController: NSViewController, AVCaptureVideoDataOutputSampleBufferDele
     }
 
     func captureOutput(captureOutput: AVCaptureOutput!, didOutputSampleBuffer sampleBuffer: CMSampleBuffer!, fromConnection connection: AVCaptureConnection!) {
-        guard let buffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
-            print("Buffer failed to get")
-            return
+        if detector == nil {
+            // Hmm, maybe the detector needs to be allocated on the same thread it is used?
+            detector = CIDetector.init(ofType: CIDetectorTypeFace, context: nil, options: [CIDetectorAccuracy: CIDetectorAccuracyHigh])
         }
-        let image = CIImage.init(CVImageBuffer: buffer)
-        let features = detector.featuresInImage(image, options: [CIDetectorSmile:true])
-        for case let feature as CIFaceFeature in features {
-//            print(feature)
-            if feature.hasSmile {
-                NSLog("smiled")
+//        NSThread.sleepForTimeInterval(10.0)
+        autoreleasepool {
+            guard let buffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {
+                print("Buffer failed to get")
+                return
+            }
+            let image = CIImage.init(CVImageBuffer: buffer)
+            let features = detector!.featuresInImage(image, options: [CIDetectorSmile:true])
+            for case let feature as CIFaceFeature in features {
+                if feature.hasSmile {
+                    NSLog("smiled")
+                }
             }
         }
-
     }
 
 }
